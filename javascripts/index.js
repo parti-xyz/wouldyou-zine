cc3ea520fc15a23af9e06450ce2df41d7ccb4d9c
ganguri <ganguri@parti.xyz>
2017-05-08 10:18:08 +0900

    index 에 월간우주당 4월호 목차를 추가합니다

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,11 +2,15 @@ $(function(){
     $(".mobile-menu").click(function(){
         $(".mobile-open-menu").slideDown();
     })
-    
+
     $("#201703 > .cover").click(function(){
         location.href="201703.html";
     })
-    
+
+    $("#201704 > .cover").click(function(){
+        location.href="201704.html";
+    })
+
     $("#closeBtn").click(function(){
         $(".mobile-open-menu").slideUp();
     })
